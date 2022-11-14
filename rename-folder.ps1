8d4754b9ef814f7934833ed2c469158c78dcdca1
Ginger Mei <ccaj007@gmail.com>
2022-11-14 16:12:32 -0500

    update

---
+++ rename-folder.ps1
@@ -0,0 +1,7 @@
+﻿$dir = "G:\_xfer\new\Blue Bloods"
+
+$folders = Get-ChildItem -Path $dir -Directory
+foreach ($folder in $folders) {
+    $new_folder = ($folder -split '\(')[0].Trim()
+    rename-item -path $dir\$folder $new_folder
+}
